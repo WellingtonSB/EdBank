cc6a267eb1439a514e9f48d1156f902ce16e971a
Wellington <71860661+WellingtonSB@users.noreply.github.com>
2021-03-08 09:22:18 -0300

    UPDATE 08/03

--- EdBank/src/Classes/ContaCorrente.java
+++ EdBank/src/Classes/ContaCorrente.java
@@ -8,13 +8,14 @@ public class ContaCorrente extends Conta {
 	Scanner ler = new Scanner(System.in);
 
 	public ContaCorrente() {
+		System.out.println("");
 		System.out.println("        Painel - Informe os dados da sua conta \n");
 		System.out.println("    ========================================== \n");
 		System.out.println("              Informe o numero da contas");
 		System.out.print("                      (4 digitos):               \n");
 		numero = ler.nextInt();
 		System.out.println("              Informe o numero do CPF:           ");
-		System.out.print("                      (8 digitos):               \n");
+		System.out.print("                      (11 digitos):               \n");
 		cpf = ler.next();
 		System.out.println("              Essa conta esta ativa?           \n");
 		System.out.println("              [1]Ativa    [2]Inativa           \n");
@@ -28,22 +29,22 @@ public class ContaCorrente extends Conta {
 	}
 
 	public void pedirTalao() {
-		System.out.println("          Deseja um tal„o de cheque ?          \n");
-		System.out.println("                 [1]Sim[2]N„o                  \n");
+		System.out.println("          Deseja um tal√£o de cheque ?          \n");
+		System.out.println("                 [1]Sim[2]N√£o                  \n");
 		System.out.println("    ========================================== \n");
 		int r = ler.nextInt();
 
 		if (r == 1) {
 			if (this.contadorTalao < 3) {
 				System.out.println("++++++++++++++++++++++++++ Extrato Conta +++++++++++++++++++++");
-				System.out.println("\t\tTransaÁ„o realizada com sucesso. ");
-				System.out.println("- N˙mero da conta            " + numero);
-				System.out.println("- OperaÁ„o          \t\t\t       cheque");
-				System.out.println("- Talıes Restantes:          " + (3 - this.contadorTalao));
+				System.out.println("\t\tTransa√ß√£o realizada com sucesso. ");
+				System.out.println("- N√∫mero da conta            " + numero);
+				System.out.println("- Opera√ß√£o          \t\t\t       cheque");
+				System.out.println("- Tal√µes Restantes:          " + (3-this.contadorTalao));
 				System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-				this.contadorTalao = this.contadorTalao + 1;
+				this.contadorTalao++;
 			} else {
-				System.out.println("Limite de tal„o excedido!");
+				System.out.println("Limite de tal√£o excedido!");
 			}
 		}
 	}
@@ -53,10 +54,10 @@ public class ContaCorrente extends Conta {
 		super.Credito(valor);
 
 		System.out.println("++++++++++++++++++++++++++ Extrato Conta +++++++++++++++++++++");
-		System.out.println("\t\tTransaÁ„o realizada com sucesso.");
-		System.out.println("- N˙mero da conta   \t\t\t " + numero);
-		System.out.println("- MovimentaÁ„o      \t\t\t R$ " + valor);
-		System.out.println("- OperaÁ„o          \t\t\t Saque");
+		System.out.println("\t\tTransa√ß√£o realizada com sucesso.");
+		System.out.println("- N√∫mero da conta   \t\t\t " + numero);
+		System.out.println("- Movimenta√ß√£o      \t\t\t R$ " + valor);
+		System.out.println("- Opera√ß√£o          \t\t\t Saque");
 		System.out.println("- Saldo Atual       \t\t\t R$" + getSaldo());
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
@@ -67,14 +68,14 @@ public class ContaCorrente extends Conta {
 		if (valor <= getSaldo()) {
 			super.Debito(valor);
 			System.out.println("++++++++++++++++++++++++++ Extrato Conta +++++++++++++++++++++");
-			System.out.println("\t\tTransaÁ„o realizada com sucesso.");
-			System.out.println("- N˙mero da conta   \t\t\t " + numero);
-			System.out.println("- MovimentaÁ„o      \t\t\t R$ " + valor);
-			System.out.println("- OperaÁ„o          \t\t\t Saque");
+			System.out.println("\t\tTransa√ß√£o realizada com sucesso.");
+			System.out.println("- N√∫mero da conta   \t\t\t " + numero);
+			System.out.println("- Movimenta√ß√£o      \t\t\t R$ " + valor);
+			System.out.println("- Opera√ß√£o          \t\t\t Saque");
 			System.out.println("- Saldo Atual       \t\t\t R$" + getSaldo());
 			System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		} else {
-			System.out.println("O dÈbito n„o foi realizado.\n\tValor insuficiente em conta.");
+			System.out.println("O d√©bito n√£o foi realizado.\n\tValor insuficiente em conta.");
 			System.out.println("     ===================================================== ");
 		}
 	}
